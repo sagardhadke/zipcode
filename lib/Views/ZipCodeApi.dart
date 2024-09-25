f5f97ace76e311b0b73d14cf24ac7bafcc5c6071
Sagar Dhadke <peaceunique2003@gmail.com>
2024-09-25 17:08:03 +0530

    Added Defult Screen

---
+++ lib/Views/ZipCodeApi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyZipCodeApi extends StatefulWidget {
+  const MyZipCodeApi({super.key});
+
+  @override
+  State<MyZipCodeApi> createState() => _MyZipCodeApiState();
+}
+
+class _MyZipCodeApiState extends State<MyZipCodeApi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
